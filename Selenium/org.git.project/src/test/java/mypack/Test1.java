a57a47b6e1e29bf38fc0e517f070bf5ebf26a2ec
Dell <Dell@DESKTOP-61MM6P5>
2021-08-03 22:41:36 +0530

    first commit

---
+++ Selenium/org.git.project/src/test/java/mypack/Test1.java
@@ -0,0 +1,14 @@
+package mypack;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		
+		int x=10;
+		int y=20;
+		int z=x+y;
+		System.out.println("addition of numbers is: " + z);
+
+	}
+
+}
