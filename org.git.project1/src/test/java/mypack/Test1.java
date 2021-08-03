7951eeff6a8d6e4dfbea3aefa85a66053692521c
Dell <Dell@DESKTOP-61MM6P5>
2021-08-03 22:04:31 +0530

    first commit

---
+++ org.git.project1/src/test/java/mypack/Test1.java
@@ -0,0 +1,15 @@
+package mypack;
+
+import org.testng.annotations.Test;
+
+public class Test1 
+{
+	@Test
+	public void method1()
+	{
+		String x="hello";
+		String y="how are you ?";
+		String w=x+y;
+		System.out.println("String after concatination:"  + w);
+	}
+}
