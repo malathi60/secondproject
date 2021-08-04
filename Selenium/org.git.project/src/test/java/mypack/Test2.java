41c9dce17030dc4760c469b2b713e443283396e3
Dell <Dell@DESKTOP-61MM6P5>
2021-08-04 13:05:03 +0530

    second commit

---
+++ Selenium/org.git.project/src/test/java/mypack/Test2.java
@@ -0,0 +1,18 @@
+package mypack;
+
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Test2 {
+
+	public static void main(String[] args) 
+	{
+			WebDriverManager.chromedriver().setup();
+			ChromeDriver driver=new ChromeDriver();
+			driver.get("https://www.google.com");
+			
+
+	}
+
+}
