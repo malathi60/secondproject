0a2fe0b1d128c06e71d401eb8e764a490fca81b8
Dell <Dell@DESKTOP-R6MJMHV>
2021-08-06 12:57:42 +0530

    commit

--- Selenium/org.git.project/src/test/java/mypack/Test2.java
+++ Selenium/org.git.project/src/test/java/mypack/Test2.java
@@ -1,6 +1,7 @@
 package mypack;
 
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 import org.openqa.selenium.firefox.FirefoxDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -9,8 +10,10 @@ public class Test2 {
 
 	public static void main(String[] args) 
 	{
-			WebDriverManager.firefoxdriver().setup();
-			FirefoxDriver driver=new FirefoxDriver();
+			WebDriverManager.chromedriver().setup();
+			ChromeOptions co=new ChromeOptions();
+			co.addArguments("--headless");               //browser is not visible but requried operations are performed
+			ChromeDriver driver=new ChromeDriver(co);
 			driver.get("https://www.google.com");
 		System.out.println(driver.getCurrentUrl());
 		
