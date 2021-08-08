2ec6c5e70dcb604e007345056b5744a207674902
malathi60 <88128247+malathi60@users.noreply.github.com>
2021-08-08 14:09:46 +0530

    Update Test1.java

--- Selenium/org.git.project/src/test/java/mypack/Test1.java
+++ Selenium/org.git.project/src/test/java/mypack/Test1.java
@@ -4,8 +4,8 @@ public class Test1 {
 
 	public static void main(String[] args) {
 		
-		int x=10;
-		int y=20;
+		int x=100;
+		int y=200;
 		int z=x+y;
 		System.out.println("sum of numbers is: " + z);
 
