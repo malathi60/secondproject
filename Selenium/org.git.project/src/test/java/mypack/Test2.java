4b25a6fce2af957c6f87e6b4189735dda6132c88
Dell <Dell@DESKTOP-R6MJMHV>
2021-08-09 16:42:50 +0530

    commit

--- Selenium/org.git.project/src/test/java/mypack/Test2.java
+++ Selenium/org.git.project/src/test/java/mypack/Test2.java
@@ -2,7 +2,9 @@ package mypack;
 
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.edge.EdgeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.opera.OperaDriver;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
@@ -10,10 +12,10 @@ public class Test2 {
 
 	public static void main(String[] args) 
 	{
-			WebDriverManager.chromedriver().setup();
-			ChromeOptions co=new ChromeOptions();
-			co.addArguments("--headless");               //browser is not visible but requried operations are performed
-			ChromeDriver driver=new ChromeDriver(co);
+			WebDriverManager.operadriver().setup();
+			//ChromeOptions co=new ChromeOptions();
+			//co.addArguments("--headless");               //browser is not visible but requried operations are performed
+			OperaDriver driver=new OperaDriver();
 			driver.get("https://www.google.com");
 		System.out.println(driver.getCurrentUrl());
 		
