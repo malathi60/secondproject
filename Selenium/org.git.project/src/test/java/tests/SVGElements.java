75a20cc71729f6df370bc2dfca0c538f650a64ff
Dell <Dell@DESKTOP-R6MJMHV>
2021-08-12 13:40:41 +0530

    commit

--- Selenium/org.git.project/src/test/java/tests/SVGElements.java
+++ Selenium/org.git.project/src/test/java/tests/SVGElements.java
@@ -1,25 +1,25 @@
 package tests;
 
 import java.time.Duration;
+
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Scanner;
-
-import javax.naming.Name;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
-import org.openqa.selenium.remote.RemoteWebDriver;
 import org.openqa.selenium.support.ui.ExpectedConditions;
 import org.openqa.selenium.support.ui.FluentWait;
+import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class SVGElements {
 
-	public static void main(String[] args) throws InterruptedException
+	@Test
+	public void methodSvg() throws InterruptedException
 	{
 		WebDriverManager.chromedriver().setup();
 		ChromeDriver driver=new ChromeDriver();
